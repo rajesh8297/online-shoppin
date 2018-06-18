ac881b4d7ef501328790873dd38b169e260e5bd4
moorthi rajesh <user@rajesh.in.com>
2018-06-18 11:48:45 +0530

    my project setup is completed succesfully

---
+++ onlinebackend/src/main/java/com/rjs/onlinebackend/App.java
@@ -0,0 +1,13 @@
+package com.rjs.onlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
